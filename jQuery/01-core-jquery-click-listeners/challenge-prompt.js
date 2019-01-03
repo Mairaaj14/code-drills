69e0b9db8a52efb9d8a4b2e8439584f2cba9cfb4
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-03 15:13:47 -0800

    add code-drills

---
+++ jQuery/01-core-jquery-click-listeners/challenge-prompt.js
@@ -0,0 +1,163 @@
+// document.ready makes sure the javascript doesn't load before the html page
+$(document).ready(function () {
+
+    // Write a series of on click listeners for each letter in the html
+    // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
+    // You will need to set up an on click listener for each letter
+    //
+
+    // Within the document, set an on click listener for the element with a class of "letterA"
+    $(document).on("click", ".letterA", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("A");
+    });
+
+    // Within the document, set an on click listener for the element with a class of "letterB"
+    $(document).on("click", ".letterB", function () {
+        // When that element is clicked, append the string "B" to the `text-div` element
+        $("#text-div").append("B");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterC"
+    $(document).on("click", ".letterC", function () {
+        // When that element is clicked, append the string "C" to the `text-div` element
+        $("#text-div").append("C");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterD"
+    $(document).on("click", ".letterD", function () {
+        // When that element is clicked, append the string "D" to the `text-div` element
+        $("#text-div").append("D");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterE"
+    $(document).on("click", ".letterE", function () {
+        // When that element is clicked, append the string "E" to the `text-div` element
+        $("#text-div").append("E");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterF"
+    $(document).on("click", ".letterF", function () {
+        // When that element is clicked, append the string "F" to the `text-div` element
+        $("#text-div").append("F");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterG"
+    $(document).on("click", ".letterG", function () {
+        // When that element is clicked, append the string "G" to the `text-div` element
+        $("#text-div").append("G");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterH"
+    $(document).on("click", ".letterH", function () {
+        // When that element is clicked, append the string "H" to the `text-div` element
+        $("#text-div").append("H");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterI"
+    $(document).on("click", ".letterI", function () {
+        // When that element is clicked, append the string "I" to the `text-div` element
+        $("#text-div").append("I");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterJ"
+    $(document).on("click", ".letterJ", function () {
+        // When that element is clicked, append the string "J" to the `text-div` element
+        $("#text-div").append("J")
+    });
+    //Within the document, set an on click listener for the element with a class of "letterK"
+    $(document).on("click", ".letterK", function () {
+        // When that element is clicked, append the string "K" to the `text-div` element
+        $("#text-div").append("K");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterL"
+    $(document).on("click", ".letterL", function () {
+        // When that element is clicked, append the string "L" to the `text-div` element
+        $("#text-div").append("L");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterM"
+    $(document).on("click", ".letterM", function () {
+        // When that element is clicked, append the string "M" to the `text-div` element
+        $("#text-div").append("M");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterN"
+    $(document).on("click", ".letterN", function () {
+        // When that element is clicked, append the string "N" to the `text-div` element
+        $("#text-div").append("N");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterO"
+    $(document).on("click", ".letterO", function () {
+        // When that element is clicked, append the string "O" to the `text-div` element
+        $("#text-div").append("O");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterP"
+    $(document).on("click", ".letterP", function () {
+        // When that element is clicked, append the string "P" to the `text-div` element
+        $("#text-div").append("P");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterQ"
+    $(document).on("click", ".letterQ", function () {
+        // When that element is clicked, append the string "Q" to the `text-div` element
+        $("#text-div").append("Q");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterR"
+    $(document).on("click", ".letterR", function () {
+        // When that element is clicked, append the string "R" to the `text-div` element
+        $("#text-div").append("R");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterS"
+    $(document).on("click", ".letterS", function () {
+        // When that element is clicked, append the string "S" to the `text-div` element
+        $("#text-div").append("S");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterT"
+    $(document).on("click", ".letterT", function () {
+      // When that element is clicked, append the string "T" to the `text-div` element
+        $("#text-div").append("T");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterU"
+    $(document).on("click", ".letterU", function () {
+        // When that element is clicked, append the string "U" to the `text-div` element
+        $("#text-div").append("U");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterV"
+    $(document).on("click", ".letterV", function () {
+        // When that element is clicked, append the string "Y" to the `text-div` element
+        $("#text-div").append("V");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterW"
+    $(document).on("click", ".letterW", function () {
+        // When that element is clicked, append the string "W" to the `text-div` element
+        $("#text-div").append("W");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterX"
+    $(document).on("click", ".letterX", function () {
+        // When that element is clicked, append the string "X" to the `text-div` element
+        $("#text-div").append("X");
+    });
+    //Within the document, set an on click listener for the element with a class of "letterY"
+    $(document).on("click", ".letterY", function () {
+        // When that element is clicked, append the string "Y" to the `text-div` element
+        $("#text-div").append("Y");
+    });
+
+    //Within the document, set an on click listener for the element with a class of "letterZ"
+    $(document).on("click", ".letterZ", function () {
+        // When that element is clicked, append the string "Z" to the `text-div` element
+        $("$text-div").append("Z");
+    });
+
+});
