05ceac1ede01caa372277fba009fe38cd10d399d
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-15 23:52:03 -0800

    constructors code-drills

--- JS-constructors/01-core-js-constructors-1/challenge-prompt.js
+++ JS-constructors/01-core-js-constructors-1/challenge-prompt.js
@@ -2,9 +2,26 @@
 //
 // -------------------- Your Code Here --------------------
 
-
-
-
+function Card(value) {
+    if (value >= 1 && value <= 13) {
+        this.value = value;
+    } else {
+        this.value = 1;
+    }
+    this.print = function() {
+        // Ternary statement
+        var printValue = this.value<10 ? "0" + this.value : this.value;
+        console.log(`
+        -----------
+        |${printValue}       |
+        |         |
+        |         |
+        |         |
+        |        ${printValue}|
+        -----------
+        `)
+    }
+}
 
 
 // --------------------- End Code Area --------------------
@@ -15,9 +32,27 @@
 //
 // -------------------- Your Code Here --------------------
 
-
-
-
+function Deck(cards = []) {
+    this.cards = cards;
+    this.count = function () {
+        return this.cards.length;
+    }
+    this.shuffle = function() {
+        for (var i = this.cards.length - 1; i >= 0; i--) {
+            var randIndex = Math.floor(Math.random()*(i+1));
+            var tempCard = this.cards[i];
+            this.cards[i] = this.cards[randIndex];
+            this.cards[randIndex] = tempCard;
+        }
+    }
+    this.draw = function () {
+        return this.cards.pop();
+    }
+    this.add = function(card) {
+        this.cards.push(card);
+        this.shuffle();
+    }
+}
 
 
 // --------------------- End Code Area --------------------
@@ -27,8 +62,7 @@
 // Export your two constructor functions
 //
 // -------------------- Your Code Here --------------------
-
-
-
+module.exports.Card = Card;
+module.exports.Deck = Deck; 
 
 // --------------------- End Code Area --------------------
